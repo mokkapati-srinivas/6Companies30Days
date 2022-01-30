2175f3bb1439845f4dfbddbc208535ae700e6d0e
Srinivas_Mokkapati <mvskmshrinivas@gmail.com>
2022-01-30 20:13:34 +0530

    Jan-30

--- Walmart/DivideTwoIntegers.java
+++ Walmart/DivideTwoIntegers.java
@@ -22,6 +22,10 @@ class Solution {
         while(div>=dis)
         {
             int shifts=0;
+            // We first double the divisor and then check because we havent made the shifts
+            // Since we havent made the shifts we have to check one ahead
+            // For example, we have to check for 3<<2 but our shifts are 1 now
+            // So we make 3<<1 => 6 and then check 6<<1 => 12 is less than or equal to dividend
             long newDis=dis<<1;
             while(div-(newDis<<shifts)>=0)
                 shifts++;
